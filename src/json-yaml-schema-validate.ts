c0ac54195aafd9ad14f23658a36ab9db63a31fc9
Nir <nir61202@gmail.com>
2020-09-22 16:36:12 +0300

    Add exclusions

--- src/json-yaml-schema-validate.ts
+++ src/json-yaml-schema-validate.ts
@@ -22,31 +22,40 @@ const argv = yargs.options({
         type: "string",
         demandOption: true,
         alias: 'd',
-        description: 'directory to validate'
+        description: 'globs to validate'
     },
     "files-type": {
         type: "string",
         default: "yaml",
         description: 'directory to validate',
         choices: ["json", "yaml"]
-    }
+    },
+    "exclusion-globs": {
+        type: "array",
+        alias: 'x',
+        description: 'globs to exclude from validation',
+        demandOption: false,
+    },
 })
     .argv;
 
 const ajv = new Ajv();
 
+let exclusionGlobs = argv["exclusion-globs"] as string[]
+
 const schemeObject = argv["schema-format"] == "json" ? JSON.parse(fs.readFileSync(argv.schema).toString()) : yaml.safeLoad(fs.readFileSync(argv.schema).toString());
 const validator = ajv.compile(schemeObject);
-glob(argv.directory,  function (err, files) {
+glob(argv.directory,  {"ignore": exclusionGlobs}, function (err, files) {
    if(err) {
        console.error(err);
        process.exit(55);
    }
    files.forEach(file => {
-       const fileContent =  argv["files-type"] == "json" ? JSON.parse(fs.readFileSync(file).toString()) : yaml.safeLoad(fs.readFileSync(file).toString());
-       if(!validator(fileContent)) {
-           console.error(validator.errors);
-           process.exit(55);
+        const fileContent =  argv["files-type"] == "json" ? JSON.parse(fs.readFileSync(file).toString()) : yaml.safeLoad(fs.readFileSync(file).toString());
+        if(!validator(fileContent)) {
+            console.log("\"" + file + "\" failed the validation.");
+            console.error(validator.errors);
+            process.exit(55);
        }
    })
 });
